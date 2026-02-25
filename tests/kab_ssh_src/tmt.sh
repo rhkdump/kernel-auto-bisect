1ab0f6450cbf4fd8e3d70b691020293a439351ae
Coiby Xu <coiby.xu@gmail.com>
2026-02-25 18:53:13 +0800

    tests: shfmt tests/kab_ssh_src/tmt.sh
    Signed-off-by: Coiby Xu <coiby.xu@gmail.com>

--- tests/kab_ssh_src/tmt.sh
+++ tests/kab_ssh_src/tmt.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 assign_server_roles() {
-    if [ -n "${TMT_TOPOLOGY_BASH}" ] && [ -f "${TMT_TOPOLOGY_BASH}" ]; then
-        # assign roles based on tmt topology data
-        # shellcheck source=/dev/null
-        . "${TMT_TOPOLOGY_BASH}"
+	if [ -n "${TMT_TOPOLOGY_BASH}" ] && [ -f "${TMT_TOPOLOGY_BASH}" ]; then
+		# assign roles based on tmt topology data
+		# shellcheck source=/dev/null
+		. "${TMT_TOPOLOGY_BASH}"
 
-        if [[ $(grep -Ec "TMT_GUESTS\[.*role\]" "${TMT_TOPOLOGY_BASH}") -gt 1 ]]; then
-            export SERVERS=${TMT_GUESTS[server.hostname]}
-            export CLIENTS=${TMT_GUESTS[client.hostname]}
-        fi
+		if [[ $(grep -Ec "TMT_GUESTS\[.*role\]" "${TMT_TOPOLOGY_BASH}") -gt 1 ]]; then
+			export SERVERS=${TMT_GUESTS[server.hostname]}
+			export CLIENTS=${TMT_GUESTS[client.hostname]}
+		fi
 
-        export HOSTNAME=${TMT_GUEST[hostname]}
-    fi
+		export HOSTNAME=${TMT_GUEST[hostname]}
+	fi
 }
 
 assign_server_roles
